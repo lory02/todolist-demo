536faffd8943df8b5fd3a4c484e4b7a386e0ac7e
chnyang <chn.yang@qq.com>
2023-03-23 13:48:28 +0800

    添加 naive-ui 组件库, 优化todolist item收藏文字变svg图标

--- src/pages/todo/todo.vue
+++ src/pages/todo/todo.vue
@@ -10,9 +10,12 @@
       <view class="todo-item" v-for="(item, index) in todoList " :key="index">
         <checkbox @click="modifyCheck(item)" :checked="item.checked" />
         <text>{{ item.text }}</text>
-        <text class="star" @click="modifyStar(item)">{{ item.star ? '收藏':'未收藏'}}</text>
+        <!-- <text class="star" @click="modifyStar(item)">{{ item.star ? '收藏':'未收藏'}}</text> -->
+        <image class="star-svg" @click="modifyStar(item)" :src="getStarSvg(item)" />
       </view>
     </view>
+
+    <!-- <image class="star-svg" src="/static/icon/xingxing_1.svg" /> -->
   </view>
 </template>
 
@@ -20,6 +23,8 @@
   export default {
     data() {
       return {
+        star0: '/static/icon/xingxing_0.svg',
+        star1: '/static/icon/xingxing_1.svg',
         inputText: '',
         todoList: [{
           text: "已完成的todo",
@@ -30,21 +35,22 @@
     },
     methods: {
       addTodo() {
-        let newData = {
+        const newData = {
           text: this.inputText,
           star: false,
           checked: false
         };
         this.todoList.push(newData);
         this.inputText = ''
-        console.log(this.todoList)
-        console.log(this.inputText)
       },
       modifyStar(item) {
         item.star = !item.star
       },
       modifyCheck(item) {
         item.checked = !item.checked
+      },
+      getStarSvg(item) {
+        return item.star ? this.star1 : this.star0
       }
     },
   }
@@ -69,4 +75,14 @@
   .star {
     margin-left: auto;
   }
+
+  .star-svg {
+    margin-left: auto;
+    width: 1.5em;
+    height: 1.5em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+  }
+
 </style>
